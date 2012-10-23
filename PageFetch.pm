02ff7a475868974cd9947c974d94668f176de7ed
Sergei Lewis <moonshadow@toothycat.net>
2012-10-23 17:38:32 +0100

    implementation

---
+++ PageFetch.pm
@@ -0,0 +1,324 @@
+package PageFetch;
+$VERSION='1.0';
+use strict;
+
+use Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT=qw(FetchPageText FetchRawPageText);
+
+local $| = 1;  # Do not buffer output (localized for mod_perl)
+
+# Configuration/constant variables:
+use vars qw(
+  $DataDir $PageDir
+  $UseSubpage $SimpleLinks $NonEnglish $FreeUpper 
+  $FreeLinkPattern $LinkPattern
+  $BNFLinkPattern $MTGLinkPattern
+  $FS $FS1 $FS2 $FS3 
+  $FreeLinks $WikiLinks 
+  );
+
+use vars qw(%Page %Section %Text
+  %KeptRevisions %IndexHash
+  %LinkIndex $MainPage
+  $OpenPageName @KeptList @IndexList $IndexInit
+  );
+
+# == Configuration =====================================================
+# Default configuration (used if UseConfig is 0)
+
+# Major options:
+$FreeLinks   = 1;       # 1 = use [[word]] links, 0 = LinkPattern only
+$WikiLinks   = 1;       # 1 = use LinkPattern,    0 = use [[word]] only
+
+# Minor options:
+$UseSubpage = 1;
+$SimpleLinks = 0;       # 1 = only letters,       0 = allow _ and numbers
+$NonEnglish  = 0;       # 1 = extra link chars,   0 = only A-Za-z chars
+$FreeUpper   = 1;       # 1 = force upper case,   0 = do not force case
+
+# ---------------------------------
+my @HtmlSingle = qw( br hr );
+my @HtmlPairs = qw( b i h1 h2 h3 blockquote pre table td tr thead tbody );
+
+sub T{
+return $_[0];
+}
+
+sub InitLinkPatterns {
+  my ($UpperLetter, $LowerLetter, $AnyLetter, $LpA, $LpB, $QDelim);
+
+  # Field separators are used in the URL-style patterns below.
+  $FS  = "\xb3";      # The FS character is a superscript "3"
+  $FS1 = $FS . "1";   # The FS values are used to separate fields
+  $FS2 = $FS . "2";   # in stored hashtables and other data structures.
+  $FS3 = $FS . "3";   # The FS character is not allowed in user data.
+
+  $OpenPageName = undef;
+
+  $UpperLetter = "[A-Z";
+  $LowerLetter = "[a-z";
+  $AnyLetter   = "[A-Za-z";
+  if ($NonEnglish) {
+    $UpperLetter .= "\xc0-\xde";
+    $LowerLetter .= "\xdf-\xff";
+    $AnyLetter   .= "\xc0-\xff";
+  }
+  if (!$SimpleLinks) {
+    $AnyLetter .= "_0-9";
+  }
+  $UpperLetter .= "-]"; $LowerLetter .= "-]"; $AnyLetter .= "-]";
+
+  # Main link pattern: lowercase between uppercase, then anything
+  $LpA = $UpperLetter . "+" . $AnyLetter . "*";
+  # Optional subpage link pattern: uppercase, lowercase, then anything
+  $LpB = $UpperLetter . "+" . $AnyLetter . "*";
+
+  # #search_in_page
+  my $LpC = "#[a-zA-Z0-9_]+";
+
+    # Loose pattern: If subpage is used, subpage may be simple name
+    $LinkPattern = "((?:(?:(?:$LpB)?\\/$LpB)|$LpA)(?:$LpC)?)";
+    # Strict pattern: both sides must be the main LinkPattern
+    # $LinkPattern = "((?:(?:$LpA)?\\/)?$LpA)";
+  $QDelim = '(?:"")?';     # Optional quote delimiter (not in output)
+  $LinkPattern .= $QDelim;
+
+  if ($FreeLinks) {
+    # Note: the - character must be first in $AnyLetter definition
+    if ($NonEnglish) {
+      $AnyLetter = "[-,.()' _0-9A-Za-z\xc0-\xff]";
+    } else {
+      $AnyLetter = "[-,.()' _0-9A-Za-z]";
+    }
+  }
+  $FreeLinkPattern = "($AnyLetter+)";
+  if ($UseSubpage) {
+    $FreeLinkPattern = "((?:(?:$AnyLetter+)?\\/)?$AnyLetter+)";
+  }
+  $FreeLinkPattern .= $QDelim;
+  $BNFLinkPattern = "BNF:" . $LinkPattern;
+  $MTGLinkPattern = "MTG:([A-Za-z0-9+-]+)";
+}
+
+sub FreeToNormal_ {
+  my ($id) = @_;
+
+  $id =~ s/ /_/g;
+  $id = ucfirst($id);
+  if (index($id, '_') > -1) {  # Quick check for any space/underscores
+    $id =~ s/__+/_/g;
+    $id =~ s/^_//;
+    $id =~ s/_$//;
+    if ($UseSubpage) {
+      $id =~ s|_/|/|g;
+      $id =~ s|/_|/|g;
+    }
+  }
+  if ($FreeUpper) {
+    # Note that letters after ' are *not* capitalized
+    if ($id =~ m|[-_.,\(\)/][a-z]|) {    # Quick check for non-canonical case
+      $id =~ s|([-_.,\(\)/])([a-z])|$1 . uc($2)|ge;
+    }
+  }
+  return $id;
+}
+
+sub OpenSection {
+  my ($name) = @_;
+
+  if (!defined($Page{$name})) {
+    $OpenPageName = 0;
+  } else {
+    %Section = split(/$FS2/, $Page{$name}, -1);
+  }
+}
+
+sub OpenText {
+  my ($name) = @_;
+
+  if (!defined($Page{"text_$name"})) {
+    $OpenPageName = 0;
+  } else {
+    &OpenSection("text_$name");
+    %Text = split(/$FS3/, $Section{'data'}, -1);
+  }
+}
+
+sub GetPageDirectory {
+  my ($id) = @_;
+
+  if ($id =~ /^([a-zA-Z])/) {
+    return uc($1);
+  }
+  return "other";
+}
+
+sub GetPageFile {
+  my ($id) = @_;
+
+  return $PageDir . "/" . &GetPageDirectory($id) . "/$id.db";
+}
+
+sub OpenPage {
+  my ($id) = @_;
+  my ($fname, $data);
+
+  # if (defined $OpenPageName && ($OpenPageName eq $id)) {
+  #   return;
+  # }
+  $OpenPageName = 0;
+  %Section = ();
+  %Text = ();
+  $fname = &GetPageFile($id);
+  if (-f $fname) {
+    $data = &ReadFile($fname);
+    %Page = split(/$FS1/, $data, -1);  # -1 keeps trailing null fields
+	$OpenPageName = $id;
+  }
+}
+
+sub ReadFile {
+  my ($fileName) = @_;
+  my ($data);
+  local $/ = undef;   # Read complete files
+
+  if (open(IN, "<$fileName")) {
+    $data=<IN>;
+    close IN;
+    return $data;
+  }
+
+  $OpenPageName = 0;
+  return "";
+}
+
+sub QuoteHtml {
+  my ($html) = @_;
+
+  $html =~ s/&/&amp;/g;
+  $html =~ s/</&lt;/g;
+  $html =~ s/>/&gt;/g;
+  if (1) {   # Make an official option?
+    $html =~ s/&amp;([#a-zA-Z0-9]+);/&$1;/g;  # Allow character references
+  }
+      my $t;
+      foreach $t (@HtmlPairs) {
+        $html =~ s/\&lt;$t((?:\s[^<>]+?)?)\&gt;(.*?)\&lt;\/$t\&gt;/<$t$1>$2<\/$t>/gis;
+      }
+
+      foreach $t (@HtmlSingle) {
+        # $html =~ s/\&lt;$t(\s[^<>]+?)\&gt;/<$t$1>/gi;
+        $html =~ s/\&lt;$t\s+\/\&gt;/<$t \/>/gi;
+        $html =~ s/\&lt;$t\&gt;/<$t>/gi;
+      }
+      my $Wiki = "http://www.toothycat.net/wiki/wiki.pl?";
+       my $BNF="http://www.toothycat.net/wiki/bnf.pl?page=";
+       my $MTG="http://www.wizards.com/magic/autocard.asp?name=";
+      $html =~ s/\[$FreeLinkPattern\s+([^\]]+?)\]/&StoreBracketLink($1, $2, $Wiki)/geos;
+      $html =~ s/\[$BNFLinkPattern\s+([^\]]+?)\]/&StoreBracketLink($1, $2, $BNF)/geos;
+      $html =~ s/$MTGLinkPattern/&StoreLink($1, 'MTG: ' . $1, $MTG)/geos;
+
+  return $html;
+}
+
+sub StoreLink
+{
+my ($id, $name, $url) = @_;
+  $id =~ s/[@"]//g;
+  $url =~ s/[@"]//g;
+ $name =~ s/[+]/ /g;
+  return "<a href=\"$url$id\">$name</a>";
+}
+
+sub StoreBracketLink
+{
+  my ($id, $name, $url) = @_;
+
+  {
+    $id = &FreeToNormal($id);
+    $name =~ s/_/ /g;
+  }
+
+  return "<a href=\"$url$id\">$name</a>";
+}
+
+sub FreeToNormal {
+  my ($id) = @_;
+
+  $id =~ s/ /_/g;
+  $id = ucfirst($id);
+  if (index($id, '_') > -1) {  # Quick check for any space/underscores
+    $id =~ s/__+/_/g;
+    $id =~ s/^_//;
+    $id =~ s/_$//;
+    
+      $id =~ s|_/|/|g;
+      $id =~ s|/_|/|g;
+  }
+  {
+    # Note that letters after ' are *not* capitalized
+    if ($id =~ m|[-_.,\(\)/][a-z]|) {    # Quick check for non-canonical case
+      $id =~ s|([-_.,\(\)/])([a-z])|$1 . uc($2)|ge;
+    }
+  }
+  return $id;
+}
+
+
+
+sub FetchPageText
+{
+  my($dir, $query) = @_;
+  $DataDir = $dir;
+  $PageDir     = "$DataDir/page";
+  &InitLinkPatterns();
+
+  if (!($query =~ /^$FreeLinkPattern$/)) 
+  {
+		return "$query: not a valid (/^$FreeLinkPattern$/) page name.";
+  }
+
+  &OpenPage($query);
+  &OpenText('default');
+  defined $Text{'text'} and $Text{'text'} =~ s/<\/?nowiki>//gi;
+  defined $Text{'text'} and $Text{'text'} =~ s/<\/?pre>//gi;
+  defined $Text{'text'} and $Text{'text'} =~ s/(<\/?)(aa)>/$1pre>/gi;
+
+  if(!$OpenPageName)
+  {
+    return "Invalid or unreified page.";
+  }
+
+  return QuoteHtml($Text{'text'});
+}
+
+sub FetchRawPageText
+{
+  my($dir, $query) = @_;
+  $DataDir = $dir;
+  $PageDir     = "$DataDir/page";
+  &InitLinkPatterns();
+
+  if (!($query =~ /^$LinkPattern$/)) 
+  {
+    if (!($FreeLinks && ($query =~ /^$FreeLinkPattern$/))) 
+	{
+		return "Not a valid page name.";
+	}
+  }
+
+  &OpenPage($query);
+  &OpenText('default');
+
+  if(!$OpenPageName)
+  {
+    return "Invalid or unreified page.";
+  }
+
+  return $Text{'text'};
+}
+
+
+# ---------------------------------
+1;
---
+++ bnf.pl
@@ -0,0 +1,453 @@
+#!/usr/bin/speedy -w -- -gwiki -M5 -r30
+use strict;
+use CGI;
+use PageFetch;
+
+use vars qw($maxiter $maxlen %options %includes %tags $validtag $commandchar);
+
+sub ResetGlobals
+{
+	# hard options
+	$maxiter = 16000; # maximum number of tags to resolve before giving up
+	$maxlen = 64000; # maximum length of generated string
+
+	# defaults for mutable options
+	%options = 
+	(
+	  'spaces' => 1,   # put spaces between elements
+	  'debug' => 0,    # generate debug trace
+	  'evil' => 0      # append Google ad
+	);
+
+	%includes = ( );
+
+	# bnf syntax
+	# name ::= |-separated-list of list of name or string or "string"
+	# root tag is bnf
+
+	# implemented as hash of array of array
+
+	%tags = ( );
+	$validtag = qw/(?:(?:(?:&lt;)[A-Za-z0-9_-]+?(?:&gt;))|(?:[A-Za-z0-9_-]+))/;
+	$commandchar = '!';
+	# commandchar must be a character that's not legal at the start of tag names (i.e. not present in $validtag)
+}
+
+sub parseBnfLine
+{
+  my ($line) = @_;
+
+  $line =~ /^ *($validtag) *::= *(.*) *$/ or return;
+
+  my $name = $1;
+  $line = $2;
+
+  if($name eq 'option')
+  {
+    if($line =~ /([A-Za-z_]+) *= *([^ ]+)/)
+	{
+	  $options{$1} = $2;
+	}
+  }
+
+  my $sequences = [ ];
+  my $curr_sequence = [ ];
+
+  while(length $line > 0)
+  {
+    $line =~ s/^ *//; # strip leading spaces
+	if($line =~ /^(\".*?[^\\]\")(.*)/)
+	{
+      # we have a quoted string as the next symbol
+	  my $symbol = $1;
+	  $line = $2;
+	  $symbol =~ s/\\(.)/$1/g;
+	  push @$curr_sequence, $symbol;
+	}
+	else
+	{
+	  my $symbol = $line;
+	  if($line =~ /^([^ ]+) (.*)$/)
+	  {
+	    $symbol = $1;
+		$line = $2;
+	  }
+	  else
+	  {
+	    $line = '';
+	  }
+	  $symbol =~ s/\\(.)/$1/g;
+
+	  if($symbol eq "|")
+	  { 
+		if($#$curr_sequence >= 0)
+		{
+	     push @$sequences, $curr_sequence;
+		 $curr_sequence = [ ];
+		}
+	  }
+	  else
+	  {
+        if($symbol =~ /^$commandchar/)
+        {
+            # they're starting a symbol with an unquoted command char
+            # we can't have that: quote it
+            # we know there's no bad effects to this because commandchar
+            # is not legal in tag names
+            $symbol = '"' . $symbol . '"';
+        }
+	    push @$curr_sequence, $symbol;
+	  }
+	}
+  }
+ 
+  if($#$curr_sequence >= 0)
+  {
+   push @$sequences, $curr_sequence;
+  }
+
+  $tags{$name} = $sequences;
+}
+
+sub dumpBNF
+{
+  foreach my $k (keys %tags)
+  {
+    print "$k ::= ";
+	my $i = -1;
+	while(++$i < $#{$tags{$k}})
+	{
+	  my $seq = $tags{$k}->[$i];
+	  foreach my $item(@$seq)
+	  {
+            my $titem = $item;
+	    if($titem =~ / /)
+		{
+		 $titem = '"' . $titem . '"';
+		}
+	    print "$titem ";
+	  }
+	  print "| ";
+	}
+
+	{
+	  my $seq = $tags{$k}->[$#{$tags{$k}}];
+	  foreach my $item(@$seq)
+	  {
+            my $titem = $item;
+	    if($titem =~ / /)
+		{
+		 $titem = '"' . $titem . '"';
+		}
+	    print "$titem ";
+	  }
+	}
+	print "<BR>\n";
+  }
+  print "<BR>\n";
+}
+
+sub resolveConcats
+{
+   my ($tag) = @_;
+
+   while($tag =~ /^($validtag)\#\#($validtag)(.*?)$/)
+   {
+     my $root = $1;
+     my $ccat = $2;
+     my $rest = '';
+     if(defined($3))
+     {
+       $rest = $3;
+     }
+     $options{'debug'} and print '<i>Concatenation:</i> '. $tag . 
+           #' (' . $root . ',' . $ccat . ',' . $rest . ')' . 
+           ' --> ';
+     if(
+        defined($tags{$ccat}) && 
+        ($#{$tags{$ccat}} == 0) && 
+        ($#{$tags{$ccat}->[0]} == 0) &&
+        ($tags{$ccat}->[0][0] =~/^$validtag$/)
+       )
+     {
+       $tag = $root . '_' . $tags{$ccat}->[0][0] . $rest;
+     }
+     else
+     {
+       $tag = $root . '_' . $ccat . $rest;
+     }
+     $options{'debug'} and print $tag;
+     $options{'debug'} and print " <BR>\n";
+   }    
+   return $tag;
+}
+
+sub generateString
+{
+  my @results = ( '' );
+  my @unresolved = ( "bnf" );
+  my $iter = 0;
+
+  while($#unresolved >= 0)
+  {
+    my $tag = shift @unresolved;
+    my $silent = '';
+    my $iterresult = [ ];
+    my @postassign = ( );
+# -------- note silent assigns and postfix assigns 
+    if($tag =~ /^&lt;($validtag(?:\#\#$validtag)*)::=($validtag(?:\#\#$validtag)*)&gt;$/)
+    {
+      # silent assign
+      $silent = resolveConcats($1);
+      $tag = $2;
+    }
+    elsif($tag =~ /^&lt;($validtag(?:\#\#$validtag)*)::=("[^"]*")&gt;$/)
+    {
+      # quoted silent assign
+      $silent = resolveConcats($1);
+      $tag = $2;
+    }
+    elsif($tag =~ /^&lt;&lt;($validtag(?:\#\#$validtag)*)::=($validtag(?:\#\#$validtag)*)&gt;&gt;$/)
+    {
+      # deep silent assign
+      $tag = $2;
+      my $deepname = resolveConcats($1);
+      $options{'debug'} and print "Start deep calculation of $tag to assign to $deepname<BR>\n";
+      my $ctag = $commandchar . 'deepname' . ' ' . $deepname;
+      unshift @unresolved, $ctag; 
+      unshift @results, '';      
+    }
+   
+    $tag =~ /^".*"$/ or $tag = resolveConcats($tag);
+  
+    # do we have any postfix assigns?
+    while($tag =~ /^($validtag){($validtag(?:\#\#$validtag)*)}(.*)$/ or $tag =~ /^($validtag){{($validtag(?:\#\#$validtag)*)}}(.*)$/)
+    {
+      if ($tag =~ /^($validtag){($validtag(?:\#\#$validtag)*)}(.*)$/)
+      {
+        # shallow postfix assign
+        push @postassign, $2; 
+        $tag = $1;
+        defined($3) and $tag = $tag . $3;
+      }
+      elsif ($tag =~ /^($validtag){{($validtag(?:\#\#$validtag)*)}}(.*)$/)
+      {
+        # deep postfix assign
+        # because we only want to output to one result at once, convert this
+        # from  foo{{bar}}  to  <<bar::=foo>> bar
+        $options{'debug'} and print "Start deep calculation of $1 to assign to $2<BR>\n";
+        unshift @unresolved, $2;     # prepend a bar
+        my $ctag = $commandchar . 'deepname' . ' ' . $2;
+        unshift @unresolved, $ctag;  # then prepend the command to assign to bar
+        unshift @results, '';      
+        $tag = $1;
+        defined($3) and $tag = $tag . $3;
+      }
+    }
+
+# -------- Resolve the tag
+
+	if (($iter++ > $maxiter) || (length($results[$#results]) > $maxlen))
+	{
+	  return $results[0] . "<BR>(...)";
+	}
+        $options{'debug'} and print "Iteration $iter: ";
+	
+    if ( $tag =~ /^($commandchar)/ )
+    {
+        # this is a command
+        #print "Found command '$tag'! ";
+        if ( $tag =~ /^($commandchar)deepname (.*)$/ )
+        {
+			# we've completed a deep assign
+			my $deepassignvar = shift(@results);
+			# all the tags come out of a deep assign flattened into a string,
+			# so we have to wrap them twice in []s to get the required array of arrays
+			$tags{$2} = [ [ $deepassignvar ] ];
+			if($options{'debug'})
+			{
+				print 'Deep assign: (' . $2 . '::=' . $deepassignvar . ")<br>\n";
+			}
+        }
+    }
+    elsif( $tags{$tag} )
+	{
+	  # select a possible sequence at random
+	  my $index = int( rand( $#{$tags{$tag}} + 1 ) );
+          defined $index or $index = 0;
+
+	  # add the items in the sequence to the unresolved list
+	  if($options{'debug'})
+          {
+            print "<i>Lookup:</i> $tag ::=";
+	    foreach my $item(@{${$tags{$tag}}[$index]})
+	    {
+	      print " $item";
+	    }
+	    print "<BR>\n";
+	  }
+	  if(defined(${$tags{$tag}}[$index])) 
+	  {
+	   foreach my $item(reverse @{${$tags{$tag}}[$index]})
+	   {
+            if($silent eq '')
+            {
+	      unshift @unresolved, $item;
+            }
+            unshift @$iterresult, $item;
+	   }
+	  }
+	}
+	else
+	{
+          my $ttag = $tag;
+	  $ttag =~ s/^"(.*)"$/$1/;
+
+          if($silent eq '')
+          {
+	    if( ((length($results[0])>0) && (length($ttag)>0) && ($options{'spaces'})) )
+	    {
+	      $results[0] .= " ";
+	    }
+	    $results[0] .= $ttag;
+          }
+          push @$iterresult, $tag;
+	}
+ 
+        if($silent ne '')
+        {
+          if($options{'debug'})
+          {
+           print '<i>Silent assign:</i> (' . $silent . '::=' ;
+           foreach my $item(@$iterresult )
+           {
+            print $item; 
+           }
+           print ")<BR>\n";
+          }
+          $tags{$silent} = [ $iterresult ];
+        }
+        # else
+        {
+          foreach my $item(@postassign)
+          {
+           if($options{'debug'})
+           {
+            print '<i>Postfix assign:</i> {' . $item . '::=' ;
+            foreach my $i(@$iterresult )
+            {
+             print $i; 
+            }
+            print "}<BR>\n";
+           }
+            $tags{$item} = [ $iterresult ];
+          }
+        }
+  }
+
+  return $results[0];
+}
+
+sub WikiLink
+{
+  my ($name) = @_;
+  return '<A HREF="http://www.toothycat.net/wiki/wiki.pl?' . 
+		$name . '">' . $name . '</A>';
+}
+
+sub main()
+{
+ ResetGlobals();
+ srand();
+ my $seed = int(rand()*(1<<31));
+ my $q = new CGI;
+ print $q->header, $q->start_html("BNF generator");
+
+ my $s = $q->param('seed'); 
+ if(defined($s) && ($s =~ /([0-9-]+)/))
+ {
+   $seed = $1;
+ }
+
+ print '<!-- Seed: ' . $seed . ' -->'."\n";
+ srand($seed);
+
+ if(defined($q->param('page')))
+ {
+    my @text = split(/[\r\n]/, FetchPageText('/home/sham/root/wiki/data', 
+       $q->param('page')));
+    
+	while($#text>-1)
+	{
+            my $item = shift @text;
+	    $item =~ /[^ ]/ or next;
+            $options{'include'} = 0;
+		&parseBnfLine($item);
+            if($options{'include'} && !defined($includes{$options{'include'}}))
+            {
+              my @newtext = split(/[\r\n]/, 
+             FetchPageText('/home/sham/root/wiki/data', $options{'include'})); 
+              @text = (@newtext, @text);
+              $includes{$options{'include'}} = 1;
+            }
+	}
+
+ if(defined($q->param('debug')))
+ {
+   $options{'debug'} = 1;
+ }
+
+	if(defined $tags{"bnf"}) # success
+	{
+		$options{'debug'} and print '<B>Here, in no particular order, are the rules I found:</B><BR>';
+		$options{'debug'} and &dumpBNF();
+		$options{'debug'} and print '<BR><HR>';
+		$options{'debug'} and print '<B>This is what I did with them:</B><BR>';
+		my $result =  &generateString();
+		$options{'debug'} and print '<BR><HR>';
+		$options{'debug'} and print '<B>..and here\'s what I got:</B><BR>';
+                print $result;
+		print '<BR><HR>';
+		print &WikiLink($q->param('page'));
+	}
+	else
+	{
+	    print 'No "bnf" tag was found. See ' . &WikiLink("MoonShadow/GeneratorGenerator") . ' for more details.<BR>';
+            #print join("<BR>", @text);
+	}
+ }
+ else
+ {
+   print 'Invoke using a link to http://www.toothycat.net/wiki/bnf.pl?page=<I>HomePage/SubPageName</I>. See ' . &WikiLink("MoonShadow/GeneratorGenerator") . ' for more details.'; 
+ }
+
+ if( $options{'evil'} )
+ {
+print <<'AD'
+<div style="float: right">
+<script type="text/javascript"><!--
+google_ad_client = "pub-3160732673995309";
+google_ad_width = 234;
+google_ad_height = 60;
+google_ad_format = "234x60_as";
+google_ad_type = "text";
+google_ad_channel ="";
+google_color_border = "EEEEEE";
+google_color_bg = "FFFFFF";
+google_color_link = "AAAAAA";
+google_color_url = "CCCCCC";
+google_color_text = "999999";
+//--></script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</div>
+AD
+;
+ }
+
+ print $q->end_html; 
+}
+
+&main();
+
